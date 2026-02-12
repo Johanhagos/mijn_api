f50e9cacd00e06b9507b371016d14538e8eb777c
johanhagos <johan_hagos@hotmail.com>
2026-02-12 21:57:20 +0100

    Fix: Remove global Header component to prevent duplicate headers on dashboard

--- merchant-dashboard/pages/_app.tsx
+++ merchant-dashboard/pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Header from '../components/Header';
 
 // Force rebuild: API_BASE fix deployment 2026-02-12
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   );
