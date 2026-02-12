d4f9d668af70008fa96fb302135f22c462684cdc
johanhagos <johan_hagos@hotmail.com>
2026-02-12 17:41:46 +0100

    Force full rebuild: API base fix

--- merchant-dashboard/pages/_app.tsx
+++ merchant-dashboard/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 
+// Force rebuild: API_BASE fix deployment 2026-02-12
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
