c5371cc018e66c254f91c10653d25c815c80bb47
johanhagos <johan_hagos@hotmail.com>
2026-02-13 19:52:50 +0100

    Add vercel.json rewrite to serve static index.html at root

--- merchant-dashboard/pages/index.tsx
+++ merchant-dashboard/pages/index.tsx
@@ -1,12 +1,3 @@
-import { useEffect } from 'react';
-
 export default function Home() {
-  useEffect(() => {
-    // Redirect to static HTML file
-    if (typeof window !== 'undefined') {
-      window.location.href = '/index.html';
-    }
-  }, []);
-
-  return <div>Loading webshop...</div>;
+  return null;
 }
