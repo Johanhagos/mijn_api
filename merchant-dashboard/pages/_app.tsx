57ddac33e59bb5f191dd383a237d7c37026ea52a
johanhagos <johan_hagos@hotmail.com>
2026-02-23 19:04:39 +0100

    Remove Analytics import - install via package.json instead

--- merchant-dashboard/pages/_app.tsx
+++ merchant-dashboard/pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
 
 // Force rebuild: API_BASE fix deployment 2026-02-12
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
